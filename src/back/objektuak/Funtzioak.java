12229151e26e1a44722554a4a231889464042147
boguminillo <boguminillo@gmail.com>
2022-05-10 11:59:30 +0200

    javadoc sin warnings

--- src/back/objektuak/Funtzioak.java
+++ src/back/objektuak/Funtzioak.java
@@ -3,5 +3,10 @@ package back.objektuak;
  * Erabiltzaile bat izan ahal dituen funtzioak
  */
 public enum Funtzioak {
-	EMPLEADO, DIRECTOR_LOCAL, DIRECTOR_PROVINCIAL
+	/** Langilea */
+	EMPLEADO,
+	/** Zuzendari lokala */
+	DIRECTOR_LOCAL,
+	/** Zuzendari erkidegoa */
+	DIRECTOR_PROVINCIAL
 }
